47f8b189433061d67020f3ebbd43108a24912b47
Elena <laskoskaelena@gmail.com>
2019-07-07 20:26:51 +0200

    Initial commit

---
+++ src/test/java/Weather.java
@@ -0,0 +1,21 @@
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Test;
+
+
+public class Weather {
+    @Test
+    public  void restAssuredexample()
+    {
+        RestAssured.baseURI="http://restapi.demoqa.com/utilities/weather/city";
+        RequestSpecification httprequest=RestAssured.given();
+        Response response=httprequest.request(Method.GET,"/Hyderabad");
+        String responseBody=response.getBody().asString();
+        System.out.println("Response body is" +responseBody);
+    }
+
+
+}
