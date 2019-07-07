a05337c9f53d33177c06d51355be2619699f5744
Elena <laskoskaelena@gmail.com>
2019-07-07 20:28:29 +0200

    Reformat Code

--- src/test/java/Weather.java
+++ src/test/java/Weather.java
@@ -8,14 +8,11 @@ import org.junit.Test;
 
 public class Weather {
     @Test
-    public  void restAssuredexample()
-    {
-        RestAssured.baseURI="http://restapi.demoqa.com/utilities/weather/city";
-        RequestSpecification httprequest=RestAssured.given();
-        Response response=httprequest.request(Method.GET,"/Hyderabad");
-        String responseBody=response.getBody().asString();
-        System.out.println("Response body is" +responseBody);
+    public void restAssuredexample() {
+        RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
+        RequestSpecification httprequest = RestAssured.given();
+        Response response = httprequest.request(Method.GET, "/Hyderabad");
+        String responseBody = response.getBody().asString();
+        System.out.println("Response body is" + responseBody);
     }
-
-
 }
